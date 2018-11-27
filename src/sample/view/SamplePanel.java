2823028c136e9c827e3c7cbb09c7efd5d2b0cf78
Van hoffman <zvan1276@740s-pl-119.local>
2018-11-27 08:22:00 -0700

    added framework to the panel

---
+++ src/sample/view/SamplePanel.java
@@ -0,0 +1,40 @@
+package sample.view;
+
+import sample.controller.GUIController;
+import javax.swing.*;
+
+public class SamplePanel extends JPanel
+{
+	private GUIController appController;
+	
+	public SamplePanel(GUIController appController)
+	{
+		super();
+		
+		this.appController = appController;
+	
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		
+	}
+
+	private void setupLayout()
+	{
+		
+	}
+
+	private void setupListeners()
+	{
+		
+	}
+	
+	
+	
+}
+
+
